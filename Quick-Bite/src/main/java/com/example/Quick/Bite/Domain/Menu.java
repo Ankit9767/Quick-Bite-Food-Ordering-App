93aea345cea50ff2f3911f0dfdf3accf316e0e96
Ankit9767 <anktmb05@gmail.com>
2025-01-11 15:00:35 +0530

    created a menu domain

---
+++ Quick-Bite/src/main/java/com/example/Quick/Bite/Domain/Menu.java
@@ -0,0 +1,77 @@
+package com.example.Quick.Bite.Domain;
+
+import com.example.Quick.Bite.Enumerations.MenuCategory;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "menus")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @JsonBackReference
+    private Restaurant restaurant;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private MenuCategory category;
+
+    public Menu() {}
+
+    public Menu(Long id, Restaurant restaurant, String name, MenuCategory category) {
+        this.id = id;
+        this.restaurant = restaurant;
+        this.name = name;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MenuCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(MenuCategory category) {
+        this.category = category;
+    }
+}
