1632dbf066a6c146451ca5a51a11a0cc4159d20e
Ankit9767 <anktmb05@gmail.com>
2025-01-12 12:34:38 +0530

    created a order domain

---
+++ Quick-Bite/src/main/java/com/example/Quick/Bite/Domain/Order.java
@@ -0,0 +1,114 @@
+package com.example.Quick.Bite.Domain;
+
+import java.util.List;
+
+import com.example.Quick.Bite.Enumerations.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private OrgUser user;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<OrderItem> orderItems;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_person_id", nullable = true)
+    private DeliveryPerson deliveryPerson;
+
+    @Column(name = "total_price", nullable = false)
+    private Double totalPrice;
+
+    @Column(name = "order_status", nullable = false)
+    private OrderStatus orderStatus;
+
+    public Order() {}
+
+    public Order(Long id, OrgUser user, Restaurant restaurant, Double totalPrice, OrderStatus orderStatus) {
+        this.id = id;
+        this.user = user;
+        this.restaurant = restaurant;
+        this.totalPrice = totalPrice;
+        this.orderStatus = orderStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OrgUser getUser() {
+        return user;
+    }
+
+    public void setUser(OrgUser user) {
+        this.user = user;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public DeliveryPerson getDeliveryPerson() {
+        return deliveryPerson;
+    }
+
+    public void setDeliveryPerson(DeliveryPerson deliveryPerson) {
+        this.deliveryPerson = deliveryPerson;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
