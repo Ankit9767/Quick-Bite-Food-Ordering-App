5781a1e204e8ec1d5c23983f9928c227b349456a
Ankit9767 <anktmb05@gmail.com>
2025-01-12 12:33:46 +0530

    created a order item domain

---
+++ Quick-Bite/src/main/java/com/example/Quick/Bite/Domain/OrderItem.java
@@ -0,0 +1,88 @@
+package com.example.Quick.Bite.Domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    @JsonBackReference
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "food_item_id", nullable = false)
+    private FoodItem foodItem;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    public OrderItem() {}
+
+    public OrderItem(Long id, Order order, FoodItem foodItem, Integer quantity, Double price) {
+        this.id = id;
+        this.order = order;
+        this.foodItem = foodItem;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public FoodItem getFoodItem() {
+		return foodItem;
+	}
+
+	public void setFoodItem(FoodItem foodItem) {
+		this.foodItem = foodItem;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+    
+}
